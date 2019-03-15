23f6b9208ad763e56dc3804a8e68449a6eb2b3d7
DavidHalman <dvdhalman@gmail.com>
2019-03-15 13:23:22 -0700

    dth - onclick changes hex color

--- src/components/board.js
+++ src/components/board.js
@@ -6,23 +6,55 @@ import './board.css';
 class Board extends Component {
     constructor(props) {
         super(props);
-        
+        const config = configs[this.props.gameSize];
+        const generator = GridGenerator.getGenerator(config.map);
+        let hexagons = generator.apply(this, config.mapProps);
+        const size = { x: config.layout.width, y: config.layout.height };
+        hexagons = hexagons.map((hex, i) => ({
+            q: hex.q,
+            r: hex.r,
+            s: hex.s,
+            color: "greyHex"
+        }))
+        this.state = {
+            config,
+            hexagons,
+            size
+        }
+    }
+
+    changeColor(event, source) {
+        const hexagons = this.state.hexagons.map(hex => {
+            // Switch pattern only for the hexagon that was clicked
+            if (HexUtils.equals(source.state.hex, hex)) {
+                // Assign new pattern to _our_ data
+                hex.color = 'redHex'
+            }
+
+            return hex;
+        });
+
+        this.setState({hexagons})
     }
 
     render() {
-        const config = configs[this.props.gameSize];
-        const generator = GridGenerator.getGenerator(config.map);
-        const hexagons = generator.apply(this, config.mapProps);
-        const size = { x: config.layout.width, y: config.layout.height };
+        const {config, hexagons, size} = this.state;
         return (
             <HexGrid width={config.width} height={config.height}>
                 <Layout size={size} flat={config.layout.flat} spacing={config.layout.spacing} origin={config.origin}>
                     {
                     // note: key must be unique between re-renders.
                     // using config.mapProps+i makes a new key when the goal template chnages.
-                    hexagons.map((hex, i) => (
-                        <Hexagon key={config.mapProps + i} q={hex.q} r={hex.r} s={hex.s} className="redHex">
-                        <Text>{HexUtils.getID(hex)}</Text>
+                    this.state.hexagons.map((hex, i) => (
+                        <Hexagon
+                            key={config.mapProps + i}
+                            q={hex.q}
+                            r={hex.r}
+                            s={hex.s}
+                            className={hex.color}
+                            onClick={(e, s) => this.changeColor(e, s)}
+                        >
+                            <Text>{HexUtils.getID(hex)}</Text>
                         </Hexagon>
                     ))
                     }
