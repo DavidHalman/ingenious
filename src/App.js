43e58330fed0ed6107ce882e6444c549b59087cb
DavidHalman <dvdhalman@gmail.com>
2019-03-18 10:03:17 -0700

    dth - alternates player turns and updating scores

--- src/App.js
+++ src/App.js
@@ -31,7 +31,8 @@ class App extends Component {
         let score = this.state.score
         //get the color o the one that was clicked
         let color = hex.color
-
+        let turn = this.state.turn
+        let player = (turn % 2 == 0) ? 0 : 1
         //go through the six directions
         var i;
 
@@ -40,7 +41,7 @@ class App extends Component {
         for(i = q + 1; i < 11 && (s + q - i) >= 0; i++){
             console.log(i + " " + r + " " + (s+q-i))
             if(hexArray[i][r][s + q - i] == color){
-                score[0][color]++
+                score[player][color]++
             } else {
                 break
             }
@@ -51,7 +52,7 @@ class App extends Component {
         for(i = q - 1; i >= 0 && (s + q - i) < 11; i--){
             console.log(i + " " + r + " " + (s+q-i))
             if(hexArray[i][r][s + q - i] == color){
-                score[0][color]++
+                score[player][color]++
             } else {
                 break
             }
@@ -62,7 +63,7 @@ class App extends Component {
         for(i = r - 1; i >= 0 && (s + r - i) < 11; i--){
             console.log(q + " " + i + " " + (s+r-i))
             if(hexArray[q][i][s + r - i] == color){
-                score[0][color]++
+                score[player][color]++
             } else {
                 break
             }
@@ -73,7 +74,7 @@ class App extends Component {
         for(i = r + 1; i < 11 && (s + r - i) >= 0; i++){
             console.log(q + " " + i + " " + (s + r - i))
             if(hexArray[q][i][s + r - i] == color){
-                score[0][color]++
+                score[player][color]++
             } else {
                 break
             }
@@ -84,7 +85,7 @@ class App extends Component {
         for(i = q - 1; i >= 0 && (q + r - i) < 11; i--){
             console.log(i + " " + (q + r- i) + " " + s)
             if(hexArray[i][q + r - i][s] == color){
-                score[0][color]++
+                score[player][color]++
             } else {
                 break
             }
@@ -94,7 +95,7 @@ class App extends Component {
         for(i = q + 1; i < 11 && (q + r - i) >= 0; i++){
             console.log(i + " " + (q + r - i) + " " + s)
             if(hexArray[i][(q + r - i)][s] == color){
-                score[0][color]++
+                score[player][color]++
             } else {
                 break
             }
@@ -103,6 +104,7 @@ class App extends Component {
         //update stqte of score
         this.setState({
             score,
+            turn: turn + 1
         })
     }
 
@@ -110,6 +112,7 @@ class App extends Component {
         return (
             <div className="App">
                 <h2>Select grid type and configuration from dropdown.</h2>
+                <h3>Turn {this.state.turn}: {(this.state.turn % 2 == 0) ? "Player 1" : "Player 2"}</h3>
                 <div>
                     <strong>Template: </strong><select onChange={(ev) => this.changeType(ev)}>
                         <option name="Two Players">Two Players</option>
